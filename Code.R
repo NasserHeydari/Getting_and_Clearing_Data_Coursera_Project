292de2b579c1909b820a7315f90b30a79cbcf375
Nasser <38111498+NasserHeydari@users.noreply.github.com>
2018-05-27 10:23:13 -0400

    Add files via upload

---
+++ Code.R
@@ -0,0 +1,70 @@
+features_path <- "C:/Users/nah315/Desktop/DataScience/GetClearData/CourseProject/UCI HAR Dataset/features.txt"
+
+x_train_path <- "C:/Users/nah315/Desktop/DataScience/GetClearData/CourseProject/UCI HAR Dataset/train/X_train.txt"
+
+y_train_path <- "C:/Users/nah315/Desktop/DataScience/GetClearData/CourseProject/UCI HAR Dataset/train/y_train.txt"
+
+subject_train_path <- "C:/Users/nah315/Desktop/DataScience/GetClearData/CourseProject/UCI HAR Dataset/train/subject_train.txt"
+
+x_test_path <- "C:/Users/nah315/Desktop/DataScience/GetClearData/CourseProject/UCI HAR Dataset/test/X_test.txt"
+
+y_test_path <- "C:/Users/nah315/Desktop/DataScience/GetClearData/CourseProject/UCI HAR Dataset/test/y_test.txt"
+
+subject_test_path <- "C:/Users/nah315/Desktop/DataScience/GetClearData/CourseProject/UCI HAR Dataset/test/subject_test.txt"
+
+activity_labels <- "C:/Users/nah315/Desktop/DataScience/GetClearData/CourseProject/UCI HAR Dataset/activity_labels.txt"
+
+#*********************************************************************************************
+
+features <- read.delim(features_path, header = FALSE, sep ="")  
+
+cnames <- t(as.data.frame(features$V2))
+
+#***********************************************Tiday Train Data************************************************
+
+train_x <- read.delim(x_train_path, header = FALSE, sep ="",dec=".")
+
+train_y <- read.delim(y_train_path, header = FALSE, sep ="",dec=".")
+
+subject_train <- read.delim(subject_train_path, header = FALSE, sep ="",dec=".")
+
+train <- cbind(subject_train,train_y, train_x)
+
+colnames(train) <- c("subject","activity",cnames)
+
+#***********************************************Tiday Test Data**************************************************
+
+test_x <- read.delim(x_test_path , header = FALSE, sep ="",dec=".")
+
+test_y <- read.delim(y_test_path, header = FALSE, sep ="",dec=".")
+
+subject_test <- read.delim(subject_test_path, header = FALSE, sep ="",dec=".")
+
+test <- cbind(subject_test,test_y, test_x)
+
+colnames(test) <- c("subject","activity",cnames)
+
+
+#************************Merges the training and the test sets to create one data set*************************
+
+Data <- rbind(test,train)    #Train and test dataset together
+
+#*******************Only the measurements on the mean and standard deviation***********************************
+
+mean_std_features <- Data[ , grepl( "subject|activity|mean|std" , names( Data ) ) ]
+
+#******************Descriptive activity names to name the activities in the data set***************************
+
+activity_labels <- read.delim(activity_labels, header = FALSE, sep ="",dec=".")  #
+
+for(i in 1: nrow(activity_labels)){mean_std_features$activity <-gsub(activity_labels[i,1],activity_labels[i,2],mean_std_features$activity) }   # this is for variable of mean and standard deviation
+
+
+#******************Average of each variable for each activity and each subject***************************
+
+mean_data_by_group <- aggregate(Data[, 3:ncol(mean_std_features)], list(mean_std_features$activity,mean_std_features$subject), mean)
+
+colnames(mean_data_by_group)[which(names(mean_data_by_group) == "Group.1")] <- "activity"
+
+colnames(mean_data_by_group)[which(names(mean_data_by_group) == "Group.2")] <- "subject"
+
